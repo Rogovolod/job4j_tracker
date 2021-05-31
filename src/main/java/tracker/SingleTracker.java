93e7ae72348c77a30d0a96520787247e5b7a6540
Rogovolod <Rogovolod@gmail.com>
2021-05-31 15:31:52 +0900

    2. Singleton [#94606]

---
+++ src/main/java/tracker/SingleTracker.java
@@ -0,0 +1,46 @@
+package tracker;
+
+import java.util.List;
+
+public final class SingleTracker {
+
+    private static SingleTracker singleTracker = null;
+
+    public static SingleTracker getInstance() {
+        if (singleTracker == null) {
+            singleTracker = new SingleTracker();
+        }
+        return singleTracker;
+    }
+
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+    /* реализовать singleton */
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(String id) {
+        return tracker.findById(id);
+    }
+
+    public List<Item> findAll() {
+        return tracker.findAll();
+    }
+
+    public List<Item> findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(String id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(String id) {
+        return tracker.delete(id);
+    }
+
+}
